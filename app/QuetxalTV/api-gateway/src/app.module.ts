b566c0ca9a187de8e82681b3fcbf3132be5e6419
Angel <angelisaias27112000@gmail.com>
2026-06-15 12:56:24 -0600

    202180003: add: primera version de archivos .yml para proyecto fase 2

--- app/QuetxalTV/api-gateway/src/app.module.ts
+++ app/QuetxalTV/api-gateway/src/app.module.ts
@@ -3,8 +3,10 @@ import { SubscriptionModule } from './subscription/subscription.module';
 import { FxModule } from './fx/fx.module';
 import { CatalogModule } from './catalog/catalog.module';
 import { AuthModule } from './auth/auth.module';
+import { HealthController } from './health.controller';
 
 @Module({
   imports: [SubscriptionModule, FxModule, CatalogModule, AuthModule],
+  controllers: [HealthController],
 })
 export class AppModule {}
