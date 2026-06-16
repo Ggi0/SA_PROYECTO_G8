b0457529ccab3fff073aa8e155be8335708e923b
Angel <angelisaias27112000@gmail.com>
2026-06-16 17:32:29 -0600

    202180003: fix: archivos yml para bd

--- app/QuetxalTV/microservices/fx-service/src/main.py
+++ app/QuetxalTV/microservices/fx-service/src/main.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "grpc"))
 
 from grpc import aio
 from dotenv import load_dotenv
